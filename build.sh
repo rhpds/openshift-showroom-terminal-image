dcf9d3ea0819ebc07abcdfc0f3a7fbe39ed9c1dc
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2023-12-09 17:21:49 +0100

    Updated image name

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ podman build . \
   --build-arg MAVEN_VERSION=${MAVEN_VERSION} \
   --build-arg OCP_VERSION=${OCP_VERSION} \
   --build-arg BUILD_DATE=${BUILD_DATE} \
-  --tag quay.io/rhpds/showroom-terminal:latest
+  --tag quay.io/rhpds/openshift-showroom-terminal-image:latest
 
-# podman tag quay.io/rhpds/showroom-terminal:latest quay.io/rhpds/showroom-terminal:${BUILD_DATE}
-# podman push quay.io/rhpds/showroom-terminal:latest
-# podman push quay.io/rhpds/showroom-terminal:${BUILD_DATE}
+# podman tag quay.io/rhpds/openshift-showroom-terminal-image:latest quay.io/rhpds/openshift-showroom-terminal-image:${BUILD_DATE}
+# podman push quay.io/rhpds/openshift-showroom-terminal-image:latest
+# podman push quay.io/rhpds/openshift-showroom-terminal-image:${BUILD_DATE}
