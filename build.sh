93d9257ae214478fb6de8c1bd2a5678f0408e5bd
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2023-12-11 14:03:28 +0100

    Base Image and dependent images

--- build.sh
+++ build.sh
@@ -1,16 +1,114 @@
 #!/bin/bash
+# -----------------------------------------------
+# Build Showroom Terminal Base Image
+# -----------------------------------------------
+TTYD_VERSION="1.7.4"
+TINI_VERSION="0.19.0"
+BUILD_DATE=$(date +"%Y-%m-%d")
+IMAGE_NAME=quay.io/rhpds/openshift-showroom-terminal-baseimage
+
+podman build . --file Dockerfile.base \
+  --build-arg TTYD_VERSION=${TTYD_VERSION} \
+  --build-arg TINI_VERSION=${TINI_VERSION} \
+  --build-arg BUILD_DATE=${BUILD_DATE} \
+  --tag ${IMAGE_NAME}:latest
+
+if [ $? -ne 0 ]; then
+  echo "*******************************************************************************"
+  echo "Error building image ${IMAGE_NAME}."
+  echo "*******************************************************************************"
+
+  exit
+fi
+
+podman tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${BUILD_DATE}
+podman push ${IMAGE_NAME}:latest
+podman push ${IMAGE_NAME}:${BUILD_DATE}
+
+# -------------------------------------------------------------------
+# Build Showroom Terminal Image for OpenShift Environments
+# -------------------------------------------------------------------
+OCP_VERSION="4.14"
+HELM_VERSION="latest"
+ODO_VERSION="v3.15.0"
+TKN_VERSION="latest"
+KN_VERSION="latest"
+VIRTCTL_VERSION="v1.1.0"
+ROXCTL_VERSION="latest"
 JAVA_VERSION=17
 MAVEN_VERSION="3.9.6"
-OCP_VERSION="4.14"
-BUILD_DATE=$(date +"%Y-%m-%d")
 
-podman build . \
+BUILD_DATE=$(date +"%Y-%m-%d")
+IMAGE_NAME=quay.io/rhpds/openshift-showroom-terminal-ocp
+
+podman build . --file Dockerfile.ocp \
+  --build-arg BUILD_DATE=${BUILD_DATE} \
   --build-arg JAVA_VERSION=${JAVA_VERSION} \
   --build-arg MAVEN_VERSION=${MAVEN_VERSION} \
   --build-arg OCP_VERSION=${OCP_VERSION} \
-  --build-arg BUILD_DATE=${BUILD_DATE} \
-  --tag quay.io/rhpds/openshift-showroom-terminal-image:latest
+  --build-arg HELM_VERSION=${HELM_VERSION} \
+  --build-arg ODO_VERSION=${ODO_VERSION} \
+  --build-arg TKN_VERSION=${TKN_VERSION} \
+  --build-arg KN_VERSION=${KN_VERSION} \
+  --build-arg VIRTCTL_VERSION=${VIRTCTL_VERSION} \
+  --build-arg ROXCTL_VERSION=${ROXCTL_VERSION} \
+  --tag ${IMAGE_NAME}:latest
 
-# podman tag quay.io/rhpds/openshift-showroom-terminal-image:latest quay.io/rhpds/openshift-showroom-terminal-image:${BUILD_DATE}
-# podman push quay.io/rhpds/openshift-showroom-terminal-image:latest
-# podman push quay.io/rhpds/openshift-showroom-terminal-image:${BUILD_DATE}
+if [ $? -ne 0 ]; then
+  echo "*******************************************************************************"
+  echo "Error building image ${IMAGE_NAME}."
+  echo "*******************************************************************************"
+
+  exit
+fi
+
+podman tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${BUILD_DATE}
+podman push ${IMAGE_NAME}:latest
+podman push ${IMAGE_NAME}:${BUILD_DATE}
+
+# -------------------------------------------------------------------
+# Build Showroom Terminal Image for ROSA Environments
+# -------------------------------------------------------------------
+ROSA_VERSION=latest
+BUILD_DATE=$(date +"%Y-%m-%d")
+IMAGE_NAME=quay.io/rhpds/openshift-showroom-terminal-rosa
+
+podman build . --file Dockerfile.rosa \
+  --build-arg BUILD_DATE=${BUILD_DATE} \
+  --build-arg ROSA_VERSION=${ROSA_VERSION} \
+  --tag ${IMAGE_NAME}:latest
+
+if [ $? -ne 0 ]; then
+  echo "*******************************************************************************"
+  echo "Error building image ${IMAGE_NAME}."
+  echo "*******************************************************************************"
+
+  exit
+fi
+
+podman tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${BUILD_DATE}
+podman push ${IMAGE_NAME}:latest
+podman push ${IMAGE_NAME}:${BUILD_DATE}
+
+# -------------------------------------------------------------------
+# Build Showroom Terminal Image for ARO Environments
+# -------------------------------------------------------------------
+BUILD_DATE=$(date +"%Y-%m-%d")
+IMAGE_NAME=quay.io/rhpds/openshift-showroom-terminal-aro
+
+podman build . --file Dockerfile.aro \
+  --build-arg BUILD_DATE=${BUILD_DATE} \
+  --build-arg ROSA_VERSION=${ROSA_VERSION} \
+  --tag ${IMAGE_NAME}:latest
+
+if [ $? -ne 0 ]; then
+  echo "*******************************************************************************"
+  echo "Error building image ${IMAGE_NAME}."
+  echo "*******************************************************************************"
+
+  exit
+fi
+
+podman tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${BUILD_DATE}
+podman push ${IMAGE_NAME}:latest
+podman push ${IMAGE_NAME}:${BUILD_DATE}
